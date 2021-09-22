a907cf2b148c78f04c04bc4499ac682b2f8e22c6
James Pero <jamespero@gmail.com>
2021-09-22 13:45:45 -0400

    adding dashboard

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,30 +1,37 @@
-import React, { lazy, Suspense, useState } from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import { StylesProvider, createGenerateClassName } from '@material-ui/core/styles';
-import Header from './components/Header';
-import Progress from './components/Progress';
+import React, { lazy, Suspense, useState } from "react";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import {
+  StylesProvider,
+  createGenerateClassName,
+} from "@material-ui/core/styles";
+import Header from "./components/Header";
+import Progress from "./components/Progress";
 
-const MarketingLazy = lazy(() => import('./components/MarketingApp'));
-const AuthLazy = lazy(() => import('./components/AuthApp'));
+const MarketingLazy = lazy(() => import("./components/MarketingApp"));
+const AuthLazy = lazy(() => import("./components/AuthApp"));
+const DashboardLazy = lazy(() => import("./components/DashboardApp"));
 
 const generateClassName = createGenerateClassName({
-  productionPrefix: 'co'
+  productionPrefix: "co",
 });
 
 export default () => {
   const [isSignedIn, setIsSignedIn] = useState(false);
 
-
-  return ( 
+  return (
     <BrowserRouter>
       <StylesProvider generateClassName={generateClassName}>
         <div>
-          <Header isSignedIn={isSignedIn} onSignOut={() => setIsSignedIn(false)} />
+          <Header
+            isSignedIn={isSignedIn}
+            onSignOut={() => setIsSignedIn(false)}
+          />
           <Suspense fallback={<Progress />}>
             <Switch>
               <Route path="/auth">
                 <AuthLazy onSignIn={() => setIsSignedIn(true)} />
               </Route>
+              <Route path="/dashboard" component={DashboardLazy} />
               <Route path="/" component={MarketingLazy} />
             </Switch>
           </Suspense>
@@ -32,4 +39,4 @@ export default () => {
       </StylesProvider>
     </BrowserRouter>
   );
-}
+};
